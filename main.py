d050844a51014061ef77583be0684988f75dad48
David Williams <davidw1457@gmail.com>
2025-07-05 21:15:08 -0500

    ch2.l1

--- main.py
+++ main.py
@@ -2,26 +2,36 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
+    verbose = False
     if len(sys.argv) < 2:
         print("no prompt provided")
         sys.exit(1)
+    elif len(sys.argv) > 2:
+        verbose = sys.argv[2] == "--verbose"
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
     client = genai.Client(api_key=api_key)
 
-    prompt = sys.argv[1]
+    user_prompt = sys.argv[1]
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
 
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=prompt
+        contents=messages
     )
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
